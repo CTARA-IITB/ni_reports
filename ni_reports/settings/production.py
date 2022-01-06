aec38e8161e7668106e7f690b5a23997db3578df
AnimeshN <animesh.nauti@gmail.com>
2022-01-06 16:26:05 +0530

    more variable

--- ni_reports/settings/production.py
+++ ni_reports/settings/production.py
@@ -1,7 +1,8 @@
 from .base import *
 
-DEBUG = False
+DEBUG = config('DEBUG', cast=bool)
 ALLOWED_HOSTS = [config('ALLOWED_HOST_1'), config('ALLOWED_HOST_2')]
+SECRET_KEY = config('SECRET_KEY')
 
 try:
     from .local import *
