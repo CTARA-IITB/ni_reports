142790901d401b455767a3d9176ad7c0ef36edeb
AnimeshN <animesh.nauti@gmail.com>
2022-01-29 18:49:49 +0530

    updated setting variable

--- ni_reports/wsgi.py
+++ ni_reports/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ni_reports.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ni_reports.settings.production")
 
 application = get_wsgi_application()
